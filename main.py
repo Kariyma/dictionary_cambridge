f8175dd8a6170d45791024a5d70354d76dff33c9
Kariyma <mariika-sun@eyandex.ru>
2022-08-03 14:14:00 +0300

    Corrected according to pep8

--- main.py
+++ main.py
@@ -1,54 +1,106 @@
-from os import path
+"""The main module of the script for the search for voicing \
+and translation of English words."""
 import argparse
+import logging
+from os import path
 
 from data import config
 from word.word import Word
 
 
-def save_file(file, word, lng):
-    filename = path.join(config.OUT_PATH, f'{word}_{lng}.mp3')
-    with open(filename, 'wb') as f:
-        f.write(file)
-    print(f"Произношение слова {word} c акцентом {lng.upper()} "
-          f"загружено в файл {filename}")
+def save_file(file_name, word, lng):
+    """Save voicing to file.
+
+    Arguments:
+        file_name: file name
+        word: word for saving
+        lng: pronounce
+    """
+    filename = path.join(config.OUT_PATH, '{0}_{1}.mp3'.format(word, lng))
+    with open(filename, 'wb') as output_file:
+        output_file.write(file_name)
+    message = 'Произношение слова {0} c акцентом {1} загружено в файл {2}'
+    logging.info(message.format(word, lng.upper()))
 
 
-def main(find: str, lng: str, more: bool):
-    word = Word(find, more)
+def check_pronunciation(word: Word, lng: str) -> bool:
+    """Check pronunciation is existing.
+
+    Arguments:
+        word: word
+        lng: pronunciation
+
+    Returns:
+        bool: result of check
+    """
     if not word.found:
         return False
     lng_dict = word.pronunciations.get(lng, None)
-    if lng_dict:
-        if not lng_dict.get('tran', None):
-            print(f"Произношение {lng} для слова {find} не найдено.")
-            return False
-    else:
-        print(f"Произношение {lng} не поддерживается.")
+    if not lng_dict:
+        logging.error('Произношение {0} не поддерживается.'.format(lng))
         return False
+    if not lng_dict.get('tran', None):
+        logging.error(
+            'Произношение {0} для слова {1} не найдено.'.format(
+                lng, word.word['en'],
+            ),
+        )
+        return False
+    return True
+
+
+def main(find: str, lng: str, more: bool) -> bool:
+    """Find word and get its voicing.
+
+    Arguments:
+        find: Word to search
+        lng: Pronunciation
+        more: Flag if need description about word
+
+    Returns:
+        bool: result
+    """
+    word = Word(find, more)
+
+    if not check_pronunciation(word, lng):
+        return False
+
     for key in word.pronunciations.keys():
-        print_it = True if not lng or key == lng else False
-        if print_it:
-            print(f"/{word.pronunciations[key]['tran']}/")
-            save_file(word.pronunciations[key]['file_content'],
-                      word.word['en'], key)
+        if not lng or key == lng:
+            logging.info('{0}'.format(word.pronunciations[key]['tran']))
+            save_file(
+                word.pronunciations[key]['file_content'],
+                word.word['en'],
+                key,
+                    )
+
     if more:
         for description in word.description:
-            print(description['en'])
-            input("continue...")
-            print(description['ru'])
-            input("continue...")
-    print(word.word['ru'])
+            logging.info(description['en'])
+            logging.info(description['ru'])
+
+    logging.info(word.word['ru'])
+
     return True
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
-    parser.add_argument('word', type=str, help="Слово для озвучивания")
+    parser.add_argument('word', type=str, help='Слово для озвучивания')
     parser.add_argument(
-        '-p', '--pronounce', type=str,
-        help="Акцент озвучивания, по умолчанию Бритаский (uk)",
-        choices=['uk', 'us'], default=config.DEFAULT_PRONUNCIATION)
-    parser.add_argument('-m', '--more', help="Выводить ли значения",
-                        action='store_true')
+        '-p',
+        '--pronounce',
+        type=str,
+        help='Акцент озвучивания, по умолчанию Бритаский (uk)',
+        choices=['uk', 'us'],
+        default=config.DEFAULT_PRONUNCIATION,
+    )
+    parser.add_argument(
+        '-m',
+        '--more',
+        help='Выводить ли значения',
+        action='store_true',
+    )
     arguments = parser.parse_args()
     main(arguments.word, arguments.pronounce, arguments.more)
--- word/word.py
+++ word/word.py
@@ -1,83 +1,163 @@
+"""The module of the class for word to searching for voicing \
+and translation of English words."""
 import logging
-from data import config
+from enum import Enum
 
 import requests
 from bs4 import BeautifulSoup
 from googletrans import Translator
 
+from data import config
 
-def translate(string):
+SOURCE = 'file'
+
+
+class Language(Enum):
+    """Enumeration for used languages."""
+
+    english = 'en'
+    russian = 'ru'
+
+
+def translate(string: str) -> str:
+    """Translate form Russian to English.
+
+    Arguments:
+        string: string to translate
+
+    Returns:
+        str: translation
+    """
     translator = Translator()
-    return translator.translate(string, src='en', dest='ru').text
+    return translator.translate(
+        string, src=Language.english, dest=Language.russian,
+    ).text
 
 
-class Word:
-    """ Класс для получения информации по слову."""
-    found = False
-    root_url = f"{config.HOST}{config.URL_ROOT}"
+class Word(object):
+    """Класс для получения информации по слову."""
+
+    root_url = '{0}{1}'.format(config.HOST, config.URL_ROOT)
     headers = config.HEADERS
-    pronunciations = {'uk': {'tran': None,
-                             'file': None,
-                             'file_content': None},
-                      'us': {'tran': None,
-                             'file': None,
-                             'file_content': None},
-                      }
-    # description = {'en': None, 'ru': None}
-    description = []
-    word = {'en': None, 'ru': None}
+    pronunciations = {
+        'uk': {
+            'tran': None,
+            'file': None,
+            'file_content': None,
+        },
+        'us': {
+            'tran': None,
+            'file': None,
+            'file_content': None,
+        },
+    }
+    error = None
+    word = {Language.english: '', Language.russian: ''}
 
-    def __init__(self, word, more=False):
-        self.word['en'] = word
-        self.word['ru'] = translate(self.word['en'])
+    def __init__(self, word: str, more: bool = False):
+        """Class for get voicing and pronunciation form word.
+
+        Arguments:
+            word: word to search
+            more: flag to get or not description
+        """
+        self.word[Language.english] = word
+        self.word[Language.russian] = translate(
+            str(self.word[Language.english]),
+        )
         self.more = more
+        self.found = False
+        self.description = []
         self.set_data()
 
-    def get_content(self):
+    def get_content(self) -> str:
+        """Get content from url.
+
+        Returns:
+            str: response
+        """
         session = requests.Session()
-        url = f"{self.root_url}{self.word['en']}"
+        url = '{0}{1}'.format(self.root_url, self.word[Language.english])
+
         try:
             response = session.get(url=url, headers=self.headers)
-            if response.status_code != 200:
-                print("Error")
-                return False
-            if response.url != url:
-                print("Word not found.")
-                return False
-            self.found = True
-            return response.text
         except OSError as err:
             logging.error(err)
-            return False
+            return ''
 
-    def get_file(self, lang):
+        response.raise_for_status()
+        self.found = True
+        if response.url != url:
+            logging.error('Word not found.')
+            return ''
+
+        return response.text
+
+    def get_file(self, lang: str) -> bytes:
+        """Get file with voicing from url.
+
+        Arguments:
+            lang: pronunciation to get
+
+        Returns:
+            bytes: file content
+        """
         session = requests.Session()
-        url = f"{config.HOST}{self.pronunciations[lang]['file']}"
+        url = '{0}{1}'.format(config.HOST, self.pronunciations[lang][SOURCE])
+
         try:
             response = session.get(url=url, headers=self.headers)
-            if response.status_code != 200:
-                print("Error")
-                return False
-            if response.url != url:
-                print("Word not found.")
-                return False
-            return response.content
         except OSError as err:
             logging.error(err)
+            return bytes(0)
+
+        response.raise_for_status()
+        if response.url != url:
+            logging.error('Word not found.')
+            return bytes(0)
+
+        return response.content
+
+    def set_pronunciations_dict(self, soup: BeautifulSoup):
+        """Set data to pronunciations dictionary.
+
+        Arguments:
+            soup: object of class BeautifulSoup
+        """
+        for tag in self.pronunciations.keys():
+            if not soup.select('span.{0}'.format(tag)):
+                continue
+            tran = soup.select(
+                'span.{0}.dpron-i > span.pron.dpron > span'.format(tag),
+                )
+            self.pronunciations[tag]['tran'] = tran[0].text if tran else None
+            audio_tag = soup.select(
+                'span.{0} amp-audio > source:nth-child(2)'.format(tag),
+            )
+            if audio_tag:
+                self.pronunciations[tag][SOURCE] = audio_tag[0].attrs['src']
+            self.pronunciations[tag]['file_content'] = self.get_file(tag)
+
+    def set_data(self) -> bool:
+        """Set data to object Word.
+
+        Returns:
+            bool: result
+        """
+        data_content = self.get_content()
+
+        if not self.found:
             return False
 
-    def set_data(self):
-        content = self.get_content()
-        if self.found:
-            soup = BeautifulSoup(content, 'lxml')
-            for tag in self.pronunciations.keys():
-                if soup.select(f"span.{tag}"):
-                    tran = soup.select(f"span.{tag}.dpron-i > span.pron.dpron > span")
-                    self.pronunciations[tag]['tran'] = tran[0].text if tran else None
-                    file = soup.select(f'span.{tag} amp-audio > source:nth-child(2)')
-                    self.pronunciations[tag]['file'] = file[0].attrs['src'] if file else None
-                    self.pronunciations[tag]['file_content'] = self.get_file(tag)
-            if self.more:
-                descriptions = soup.select('#page-content div.ddef_h > div')
-                self.description = [{'en': description.text, 'ru': translate(description.text)}
-                                    for description in descriptions]
+        soup = BeautifulSoup(data_content, 'lxml')
+
+        self.set_pronunciations_dict(soup)
+
+        if self.more:
+            descriptions = soup.select('#page-content div.ddef_h > div')
+            self.description = [
+                {
+                    Language.english: description.text,
+                    Language.russian: translate(description.text),
+                } for description in descriptions
+            ]
